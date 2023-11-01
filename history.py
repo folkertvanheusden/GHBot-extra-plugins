8713c2032e413491c3f5e6cdd85e68ef4a168d98
Folkert van Heusden <folkert@vanheusden.com>
2023-11-01 09:03:08 +0100

    !firstseen

--- history.py
+++ history.py
@@ -17,6 +17,7 @@ mqtt_port    = 18830
 topic_prefix = 'kiki-ng/'
 channels     = ['test', 'knageroe']
 db_file      = 'history.db'
+prefix       = '!'
 
 con = sqlite3.connect(db_file)
 
@@ -44,13 +45,27 @@ def init_db():
 
     con.commit()
 
+def announce_commands(client):
+    target_topic = f'{topic_prefix}to/bot/register'
+
+    client.publish(target_topic, 'cmd=firstseen|descr=when was a person first seen')
+
 def on_message(client, userdata, message):
     global history
+    global prefix
 
     try:
         text = message.payload.decode('utf-8')
 
         topic = message.topic[len(topic_prefix):]
+
+        if topic == 'from/bot/command' and text == 'register':
+            announce_commands(client)
+            return
+
+        if topic == 'from/bot/parameter/prefix':
+            prefix = text
+            return
 
         parts   = topic.split('/')
         channel = parts[2] if len(parts) >= 3 else 'nurds'
@@ -70,11 +85,37 @@ def on_message(client, userdata, message):
 
             con.commit()
 
+            tokens  = text.split(' ')
+
+            command = tokens[0][1:]
+
+            response_topic = f'{topic_prefix}to/irc/{channel}/notice'
+
+            if command == 'firstseen' and tokens[0][0] == prefix and len(tokens) >= 2:
+                cur = con.cursor()
+                cur.execute('SELECT what, `when` FROM history WHERE channel=? AND nick=? ORDER BY `when` ASC LIMIT 1', (channel.lower(), tokens[1].lower()))
+                result = cur.fetchone()
+                cur.close()
+
+                client.publish(response_topic, f'It was {result[1]} when {tokens[1]} said "{result[0]}"')
+
     except Exception as e:
         print(f'{e}, line number: {e.__traceback__.tb_lineno}')
 
 def on_connect(client, userdata, flags, rc):
     client.subscribe(f'{topic_prefix}from/irc/#')
+
+    client.subscribe(f'{topic_prefix}from/bot/command')
+
+def announce_thread(client):
+    while True:
+        try:
+            announce_commands(client)
+
+            time.sleep(4.1)
+
+        except Exception as e:
+            print(f'Failed to announce: {e}')
 
 init_db()
 
@@ -82,5 +123,8 @@ client = mqtt.Client(f'{socket.gethostname()}_{sys.argv[0]}', clean_session=Fals
 client.on_message = on_message
 client.on_connect = on_connect
 client.connect(mqtt_server, port=mqtt_port, keepalive=4, bind_address="")
+
+t = threading.Thread(target=announce_thread, args=(client,))
+t.start()
 
 client.loop_forever()
