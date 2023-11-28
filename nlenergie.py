b8ca374831729329345edcf45c0acb6e3f6a944a
Folkert van Heusden <folkert@vanheusden.com>
2023-11-28 12:44:20 +0100

    price bargraph

--- nlenergie.py
+++ nlenergie.py
@@ -26,13 +26,65 @@ prefix       = '!'  # !command, will be updated by ghbot
 
 netherlands_tz = pytz.timezone("Europe/Amsterdam")
 
+bar = chr(9601) + chr(9602) + chr(9603) + chr(9604) + chr(9605) + chr(9606) + chr(9607) + chr(9608)
+barcount = len(bar)
+
 prev_j = None
 prev_j2 = None
+
+data_gen = []
+data_price = []
+
+def collect_thread():
+    while True:
+        try:
+            headers = { 'User-Agent': 'Kiki' }
+            r = requests.get('http://stofradar.nl:9001/electricity/generation', timeout=2, headers=headers)
+            r2 = requests.get('http://stofradar.nl:9001/electricity/price', timeout=2, headers=headers)
+
+            j = json.loads(r.content.decode('ascii'))
+            data_gen.append(j)
+
+            j2 = json.loads(r2.content.decode('ascii'))
+            price = j2['current']['price']
+            data_price.append(price)
+
+            while len(data_gen) > 288:
+                del data_gen[0]
+                del data_price[0]
+
+            time.sleep(300)
+
+        except:
+            print(f'Exception during "nlenergie": {e}, line number: {e.__traceback__.tb_lineno}')
+            # gives skew
+            time.sleep(5)
 
 def announce_commands(client):
     target_topic = f'{topic_prefix}to/bot/register'
 
     client.publish(target_topic, 'cmd=nlenergie|descr=Stroomgebruik in Nederland op dit moment')
+
+def name_to_color(name):
+    if name == 'solar':
+        return 8
+
+    elif name == 'wind':
+        return 12
+
+    elif name == 'nuclear':
+        return 9
+
+    elif name == 'waste':
+        return 15
+
+    elif name == 'other':
+        return 6
+
+    elif name == 'fossil':
+        return 5
+
+    return (abs(hash(name) * 9) % 13) + 2
 
 def on_message(client, userdata, message):
     global prefix
@@ -73,7 +125,8 @@ def on_message(client, userdata, message):
                 parts = text.split()
 
                 verbose = '-v' in parts
-                very_verbose = '-vv' in parts
+                very_very_verbose = '-vvv' in parts
+                very_verbose = ('-vv' in parts) or very_very_verbose
 
                 headers = { 'User-Agent': 'Kiki' }
 
@@ -112,26 +165,7 @@ def on_message(client, userdata, message):
                     perc = source['power'] * 100.0 / j['total']
                     not_perc = source['power'] * 40.0 / j['total']
 
-                    if source['id'] == 'solar':
-                        color_index = 8
-
-                    elif source['id'] == 'wind':
-                        color_index = 12
-
-                    elif source['id'] == 'nuclear':
-                        color_index = 9
-
-                    elif source['id'] == 'waste':
-                        color_index = 15
-
-                    elif source['id'] == 'other':
-                        color_index = 6
-
-                    elif source['id'] == 'fossil':
-                        color_index = 5
-
-                    else:
-                        color_index = (abs(hash(source['color']) * 9) % 13) + 2
+                    color_index = name_to_color(source['id'])
 
                     out += f"\3{color_index}{source['id']}: {source['power']} MW ({perc:.2f}%"
 
@@ -149,11 +183,43 @@ def on_message(client, userdata, message):
 
                 out += f' ({ts})'
 
+                sparkline = ''
+                print(data_gen, data_price)
+                if very_very_verbose and len(data_gen) >= 2:
+                    values = []
+                    colors = []
+
+                    n_items = len(data_gen)
+                    for i in range(max(0, n_items - 20), n_items):
+                        best_value  = -1
+                        best_name  = None
+                        for source in data_gen[i]['mix']:
+                            value = source['power'] * data_price[i]
+                            if value > best_value:
+                                best_value = value
+                                best_name = source['id']
+
+                        values.append(best_value)
+                        colors.append(name_to_color(best_name))
+
+                    highest_value = max(values)
+                    lowest_value = min(values)
+                    print(values, lowest_value, highest_value)
+
+                    extent = highest_value - lowest_value
+
+                    if extent != 0:
+                        sparkline = ' '
+                        for i in range(len(values)):
+                            sparkline += f'\3{colors[i]}'
+                            sparkline += bar[min([barcount - 1, int((values[i] - lowest_value) / extent * barcount)])]
+                        sparkline += '\3'
+
                 if verbose:
-                    client.publish(response_topic, outblocks + f' ({ts} / {outblocks_l})')
+                    client.publish(response_topic, outblocks + f' ({ts} / {outblocks_l})' + sparkline)
 
                 else:
-                    client.publish(response_topic, out)
+                    client.publish(response_topic, out + sparkline)
 
             except Exception as e:
                 client.publish(response_topic, f'Exception during "nlenergie": {e}, line number: {e.__traceback__.tb_lineno}')
@@ -178,7 +244,10 @@ client.on_message = on_message
 client.on_connect = on_connect
 client.connect(mqtt_server, port=mqtt_port, keepalive=4, bind_address="")
 
-t = threading.Thread(target=announce_thread, args=(client,))
+t = threading.Thread(target=collect_thread)
 t.start()
+
+t2 = threading.Thread(target=announce_thread, args=(client,))
+t2.start()
 
 client.loop_forever()
